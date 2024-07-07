866e288886ce8d05a128c052a1fb135f3e56518e
Rodrigue Daassi <68090033+RodrigueDaassi@users.noreply.github.com>
2024-07-06 21:48:08 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,18 +2,18 @@ function ReadMore() {
   var dots = document.getElementById("dots");
   var moreText = document.getElementById("more");
   var btnText = document.getElementById("myBtn");
+  var lang = document.documentElement.lang;
 
   if (dots.style.display === "none") {
     dots.style.display = "inline";
-    btnText.innerHTML = "More news";
+    btnText.innerHTML = lang === "fr" ? "Plus de nouvelles" : "More news";
     moreText.style.display = "none";
   } else {
     dots.style.display = "none";
-    btnText.innerHTML = "Reduce";
+    btnText.innerHTML = lang === "fr" ? "Réduire" : "Reduce";
     moreText.style.display = "inline";
   }
 }
-
 
 function myMenu() {
   var x = document.getElementById("Menu");
